b4a3ae52811f3d519bca7f128f551c240065d946
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-01-12 23:38:06 -0600

    update lsp

--- nvim/.config/nvim/lua/thenameiswiiwin/lsp/init.lua
+++ nvim/.config/nvim/lua/thenameiswiiwin/lsp/init.lua
@@ -1,6 +1,62 @@
-local nvim_lsp = require('lspconfig')
-local servers = { 'tsserver', 'tailwindcss' }
+local lspconfig = require("lspconfig")
+local configs = require "lspconfig.configs"
 
-for _, lsp in ipairs(servers) do 
-    nvim_lsp[lsp].setup {}
+local lsp_status = require("lsp-status")
+lsp_status.register_progress()
+
+local on_attach = function(client, bufnr)
+
+  lsp_status.on_attach(client)
+  mapBuf(bufnr, "n", "<Leader>gdc", "<Cmd>lua vim.lsp.buf.declaration()<CR>")
+  mapBuf(bufnr, "n", "<Leader>gd", "<Cmd>lua vim.lsp.buf.definition()<CR>")
+  mapBuf(bufnr, "n", "<Leader>gh", "<Cmd>lua vim.lsp.buf.hover()<CR>")
+  mapBuf(bufnr, "n", "<Leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  mapBuf(bufnr, "n", "<Leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  mapBuf(bufnr, "n", "<Leader>gtd", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  mapBuf(bufnr, "n", "<Leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  mapBuf(bufnr, "n", "<Leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  -- mapBuf(bufnr, "n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  mapBuf(bufnr, "n", "<Leader>ca", "<cmd> lua require('mh.telescope').code_actions()<cr>")
+  mapBuf(bufnr, "v", "<Leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>")
+  mapBuf(bufnr, "n", "<Leader>sd", "<cmd>lua vim.diagnostic.open_float(0, { scope = 'line' })<CR>")
+  -- autocmd("CrsorHold", "<buffer>", "lua vim.diagnostic.show_position_diagnostics({focusable=false})")u
+
+  vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+
+  vim.fn.sign_define("DiagnosticSignError", {text = "•", texthl = "DiagnosticSignError"})
+  vim.fn.sign_define("DiagnosticSignWarn", {text = "•", texthl = "DiagnosticSignWarn"})
+  vim.fn.sign_define("DiagnosticSignInfo", {text = "•", texthl = "DiagnosticSignInfo"})
+  vim.fn.sign_define("DiagnosticSignHint", {text = "•", texthl = "DiagnosticSignHint"})
+
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_command("autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()")
+    vim.api.nvim_command("autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()")
+    vim.api.nvim_command("autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()")
+  end
 end
+
+local servers = { "tsserver", "tailwindcss", "html", "cssls", "bashls", "vimls" }
+for _, lsp in ipairs(servers) do 
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities
+    }
+end
+lspconfig.sourcekit.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+configs.emmet_ls = {
+  default_config = {
+    cmd = {"emmet-ls", "--stdio"},
+    filetypes = {"html", "css", "scss"},
+    root_dir = function()
+      return vim.loop.cwd()
+    end,
+    settings = {}
+  }
+}
+lspconfig.emmet_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
