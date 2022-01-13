0dfdea0ae3dd2bf5cd9fa8631f2ac1b14b732c90
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-01-12 23:21:56 -0600

    fix bug

--- nvim/.config/nvim/lua/thenameiswiiwin/lsp/init.lua
+++ nvim/.config/nvim/lua/thenameiswiiwin/lsp/init.lua
@@ -1,1 +1,6 @@
-require('lspconfig').tsserver.setup{}
+local nvim_lsp = require('lspconfig')
+local servers = { 'tsserver', 'tailwindcss' }
+
+for _, lsp in ipairs(servers) do 
+    nvim_lsp[lsp].setup {}
+end
