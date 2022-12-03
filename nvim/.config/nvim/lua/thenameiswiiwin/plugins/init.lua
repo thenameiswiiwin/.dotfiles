160729eb5b7ed6d75edafccb20887aae906c4e9b
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-12-02 19:45:50 -0600

    update

--- nvim/.config/nvim/lua/thenameiswiiwin/plugins/init.lua
+++ nvim/.config/nvim/lua/thenameiswiiwin/plugins/init.lua
@@ -344,7 +344,5 @@ return require "packer".startup(
     use {"mjlbach/neovim-ui"}
     use {"MunifTanjim/nui.nvim"}
     use {"dstein64/vim-startuptime"}
-
-    use {"manzeloth/live-server"}
   end
 )
