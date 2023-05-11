99bab21889e9cc8440028781a5917c21940f4df6
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-05-11 16:08:18 -0500

    update

--- nvim/.config/nvim/lua/thenameiswiiwin/autocmds/init.lua
+++ nvim/.config/nvim/lua/thenameiswiiwin/autocmds/init.lua
@@ -1,17 +1,17 @@
 local H = {}
 function H.autocmd(event, triggers, operations)
-  local cmd = string.format("autocmd %s %s %s", event, triggers, operations)
- vim.cmd(cmd)
+	local cmd = string.format("autocmd %s %s %s", event, triggers, operations)
+	vim.cmd(cmd)
 end
 
-H.autocmd("BufEnter",     "*",   "if &buftype == 'terminal' | :startinsert | endif")
-H.autocmd("BufReadPost",  "*",   [[if line("'\"") > 0 && line ("'\"") <= line("$") | exe "normal! g'\"" | endif]])
+H.autocmd("BufEnter", "*", "if &buftype == 'terminal' | :startinsert | endif")
+H.autocmd("BufReadPost", "*", [[if line("'\"") > 0 && line ("'\"") <= line("$") | exe "normal! g'\"" | endif]])
 -- H.autocmd("BufWritePre",  "*",   "%s/\\s\\+$//e")
 
-H.autocmd("ColorScheme",  "*",   "lua require('thenameiswiiwin.colors').setItalics()")
-H.autocmd("CompleteDone", "*",   "pclose")
+H.autocmd("ColorScheme", "*", "lua require('thenameiswiiwin.colors').setItalics()")
+H.autocmd("CompleteDone", "*", "pclose")
 
-H.autocmd("FileType",     "vue", "syntax sync fromstart")
+H.autocmd("FileType", "vue", "syntax sync fromstart")
 
 -- H.autocmd("InsertEnter",  "*",   "let save_cwd = getcwd() | set autochdir")
 -- H.autocmd("InsertEnter",  "*",   "if !exists('w:last_fdm') | let w:last_fdm=&foldmethod | setlocal foldmethod=manual | endif")
@@ -19,12 +19,11 @@ H.autocmd("FileType",     "vue", "syntax sync fromstart")
 -- H.autocmd("InsertLeave",  "*",   "set noautochdir | execute 'cd' fnameescape(save_cwd)")
 -- H.autocmd("InsertLeave",  "*",   "if exists('w:last_fdm') | let &l:foldmethod=w:last_fdm | unlet w:last_fdm | endif")
 
-H.autocmd("TermOpen",     "*",   "setl bufhidden=hide")
-H.autocmd("TermOpen",     "*",   "startinsert")
-H.autocmd("TermOpen",     "*",   "setl nonumber")
+H.autocmd("TermOpen", "*", "setl bufhidden=hide")
+H.autocmd("TermOpen", "*", "startinsert")
+H.autocmd("TermOpen", "*", "setl nonumber")
 
-H.autocmd("WinLeave",     "*",   "if exists('w:last_fdm') | let &l:foldmethod=w:last_fdm | unlet w:last_fdm | endif")
-
+H.autocmd("WinLeave", "*", "if exists('w:last_fdm') | let &l:foldmethod=w:last_fdm | unlet w:last_fdm | endif")
 
 -- vim.api.nvim_command('autocmd WinEnter * v:lua.thenameiswiiwin.autocmds.Preview_func()')
 
